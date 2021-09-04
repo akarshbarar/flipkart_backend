898e2f4ee1baa6ccc434d0731fa92866cebdc601
Akarsh Barar <barar.akarsh8@gmail.com>
2021-09-04 22:12:46 +0530

    Rest api

--- server.js
+++ server.js
@@ -1,19 +1,59 @@
 const express = require("express")
-const Firebase = require("firebase");
 const app = express()
 app.use(express.json())
-var admin = require("firebase-admin");
+// Import the functions you need from the SDKs you need
 
-var serviceAccount = require("./secretKey.json");
+const { initializeApp } = require("firebase/app");
+const { getAuth, signInWithPhoneNumber } =  require("firebase/auth");
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
+// TODO: Add SDKs for Firebase products that you want to use
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyDBGag6B5CnoXg1DdiSBNhA5oFFPlgroV0",
+
+  authDomain: "flipkartcloneapp.firebaseapp.com",
+
+  projectId: "flipkartcloneapp",
+
+  storageBucket: "flipkartcloneapp.appspot.com",
+
+  messagingSenderId: "619526600616",
+
+  appId: "1:619526600616:web:83d06eeaa4f9ee29384f9d"
+
+};
+
+
+// Initialize Firebase
+
+
+
 
 app.post("/getOTP",(req,res)=>{
-
-    let mobileNumber = req.body.mobileNumber;
     
+    let mobileNumber = req.body.mobileNumber;
+    const firebase = initializeApp(firebaseConfig);
+const phoneNumber = "+919876543210";//NOT valid number
+const appVerifier = true;
+
+const auth = getAuth();
+signInWithPhoneNumber(auth, phoneNumber, appVerifier)
+    .then((confirmationResult) => {
+      // SMS sent. Prompt user to type the code from the message, then sign the
+      // user in with confirmationResult.confirm(code).
+        console.log(confirmationResult)
+      // ...
+    }).catch((error) => {
+      // Error; SMS not sent
+      // ...
+    });
+
     res.json({
         message: "OTP SEND"
     })
